ec34eab382f78943b3ea0a64d43792cc51aa6109
Vaibhav Aggarwal <aggarwalvaibhav0908@gmail.com>
2026-08-24 20:17:00 +0530

    Add basic stack operations in basics.cpp
    Demonstrate basic stack operations including push, emplace, pop, and swap.

---
+++ 03-Stack/basics.cpp
@@ -0,0 +1,10 @@
+//declaration (LIFO)
+stack<int> st;
+st.push(1);
+st.emplace(5);
+cout<< st.top(); //prints 5; index accessing not allowed in stack
+st.pop();//pops 5
+
+//swapping s1 with s2
+stack<int> st1,st2;
+st1.swap(s2);
